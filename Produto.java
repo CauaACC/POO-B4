3e8413fa2227a6c0169cdda1f6faaec754b592c6
Cauak <caua.crosati@edu.unifil.br>
2024-10-10 10:04:17 -0300

    Sistema de Gestão de produtos

---
+++ Produto.java
@@ -0,0 +1,35 @@
+abstract class Produto {
+
+    protected String nome;
+    protected double preco;
+
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+
+    public abstract double calcularDesconto(double porcentagem);
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public double getPreco() {
+        return preco;
+    }
+
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+}
---
+++ Tipo2.java
@@ -0,0 +1,14 @@
+class Tipo2 extends Produto {
+
+    public Tipo2(String nome, double preco) {
+        super(nome, preco);
+    }
+
+    @Override
+    public double calcularDesconto(double porcentagem) {
+        if (porcentagem > 10) {
+            porcentagem = 10;
+        }
+        return preco - (preco * (porcentagem / 100));
+    }
+}
