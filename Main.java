10d165f981ec3df7f988bf08d7e67aede4fc6b24
Cauak <caua.crosati@edu.unifil.br>
2024-11-06 17:01:36 -0300

    Livro_Solid_05/11

--- Main.java
+++ Main.java
@@ -1,17 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Gerente gerente = new Gerente("Roberto");
-        Desenvolvedor dev = new Desenvolvedor("Claudio");
-
-        System.out.println("Informções do gerente:");
-        System.out.println("nome: " + gerente.nome);
-        System.out.println("Função: " + gerente.descreverResponsabilidades());
-        System.out.println("Salário: " + gerente.calcularSalario());
-        System.out.println("------------------------");
-        System.out.println("Informações do desenvolvedor:");
-        System.out.println("nome: " + dev.nome);
-        System.out.println("Função: " + dev.descreverResponsabilidades());
-        System.out.println("Salário: " + dev.calcularSalario());
-
+        Livro livro = new Livro("Davi Veiga", "Java para iniciantes");
+        RepositorioLivro repositorio = new RepositorioLivro();
+        repositorio.salvar(livro);
     }
 }
